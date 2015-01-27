0bd7b071931d3fc8d385e825ff584c2dc3d09f15
Oh233 <macshqi@gmail.com>
2015-01-27 12:54:07 +0800

    add template

--- lecture/quicksort.cpp
+++ lecture/quicksort.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void swap(int &a,int &b) {
-  int tmp = a;
+template<typename T>
+void _swap(T &a,T &b) {
+  T tmp = a;
   a = b;
   b = tmp;
 }
 
-int partition(int* A, int left, int right) {
-  int pivot = A[right];
+template<typename T>
+int partition(T* A, int left, int right) {
+  T pivot = A[right];
   int i = left-1;
   int j = left;
   for (j = left;j<right;j++) {
     if (A[j] < pivot) {
       i++;
-      swap(A[i],A[j]);
+      _swap(A[i],A[j]);
     }
   }
-  swap(A[i+1],A[j]);
+  _swap(A[i+1],A[j]);
   return i+1;
 }
 
-void quicksort(int* A, int left, int right) {
+template<typename T>
+void quicksort(T* A, int left, int right) {
   if (left < right) {;
     int q = partition(A,left,right);
     quicksort(A,left,q-1);
@@ -31,10 +34,18 @@ void quicksort(int* A, int left, int right) {
 
 int main() {
   cout << "welcome to quicksort" << endl;
+  cout << "sort for integers" << endl;
   int a[9] = {7,9,11,3,5,2,6,7,10};
   quicksort(a,0,8);
   for (int i=0;i<8;i++) {
     cout << a[i] << " ";
+  }
+  cout << endl;
+  cout << "sort for charactors" << endl;
+  char b[9] = {'c','e','k','i','s','j','a','l','o'};
+  quicksort(b,0,8);
+  for (int i=0;i<8;i++) {
+    cout << b[i] << " ";
   }
   return 0;
 }
